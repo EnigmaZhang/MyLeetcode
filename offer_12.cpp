b47d837841ae9d5b306b05a5b7cc0b4f576180d0
Enigma <40985011+EnigmaZhang@users.noreply.github.com>
2021-06-08 22:41:21 +0800

    offer_13

--- offer_12.cpp
+++ offer_12.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include <string>
 
@@ -7,5 +6,56 @@ class Solution
 public:
     bool exist(std::vector<std::vector<char>> &board, std::string word)
     {
+        bool* visited = new bool[board.size() * board[0].size()];
+        for (int i{ 0 }; i < board.size() * board[0].size(); i++)
+        {
+            visited[i] = false;
+        }
+
+        int pathLength = 0;
+
+        for (size_t row{ 0 }; row < board.size(); row++)
+        {
+            for (size_t col { 0 }; col < board[0].size(); col++)
+            {
+                if (hasPathCore(board, row, col, word, pathLength, visited))
+                {
+                    return true;
+                }
+            }
+            
+        }
+
+        delete[] visited;
+
+        return false;
+    }
+
+    bool hasPathCore(const std::vector<std::vector<char>> &board, size_t row, size_t col, std::string word, int& pathLength, bool* visited)
+    {
+        if (word.length() == pathLength)
+        {
+            return true;
+        }
+        bool hasPath = false;
+
+        if (row >= 0 && row < board.size() && col >= 0 && col < board[0].size() && board[row][col] == word[pathLength] && !visited[board[0].size() * row + col])
+        {
+            pathLength++;
+            visited[board[0].size() * row + col] = true;
+
+            hasPath = hasPathCore(board, row - 1, col, word, pathLength, visited)
+            || hasPathCore(board, row + 1, col, word, pathLength, visited)
+            || hasPathCore(board, row, col + 1, word, pathLength, visited)
+            || hasPathCore(board, row, col - 1, word, pathLength, visited);
+
+            if (!hasPath)
+            {
+                pathLength--;
+                visited[board[0].size() * row + col] = false;
+            }
+        }
+
+        return hasPath;
     }
 };
